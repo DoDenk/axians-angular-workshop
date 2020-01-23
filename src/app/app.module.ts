ac060a36b3cb325991b7b75a08fe8374fdc2a08f
Dominic Denker <dominic.denker@axians.de>
2020-01-23 10:09:56 +0100

    ex1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,7 @@
+import { OverviewComponent } from './exercise/overview/overview.component';
+import { WorkshopAgendaComponent } from './interactive-routing/workshop-agenda/workshop-agenda.component';
+import { WorkshopPageTwoComponent } from './interactive-routing/workshop-page-two/workshop-page-two.component';
+import { WorkshopPageOneComponent } from './interactive-routing/workshop-page-one/workshop-page-one.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -6,7 +10,11 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+		WorkshopPageOneComponent,
+		WorkshopPageTwoComponent,
+		WorkshopAgendaComponent,
+		OverviewComponent
   ],
   imports: [
     BrowserModule,
