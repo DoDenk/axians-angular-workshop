8d66f49822f55f6f135439fcadb91167885ee334
Dominic Denker <dominic.denker@axians.de>
2020-01-23 10:09:56 +0100

    exercise1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,36 @@
+import { WorkshopAgendaComponent } from './interactive-routing/workshop-agenda/workshop-agenda.component';
+import { WorkshopPageTwoComponent } from './interactive-routing/workshop-page-two/workshop-page-two.component';
+import { WorkshopPageOneComponent } from './interactive-routing/workshop-page-one/workshop-page-one.component';
+import { OverviewComponent } from './exercise/overview/overview.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  redirectTo: 'exercise',
+  pathMatch: 'full'
+},
+{
+  path: 'exercise',
+  component: OverviewComponent
+},
+{
+  path: 'page1',
+  component: WorkshopPageOneComponent
+},
+{
+  path: 'page2',
+  component: WorkshopPageTwoComponent
+},
+{
+  path: 'agenda',
+  component: WorkshopAgendaComponent
+},
+{
+  path: '**',
+  redirectTo: 'home'
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
