581812bcfde0e53cb92782a9691a90529a11991c
Dominic Denker <dominic.denker@axians.de>
2020-01-23 10:05:22 +0100

    base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,18 @@
-import { WorkshopAgendaComponent } from './interactive-routing/workshop-agenda/workshop-agenda.component';
-import { WorkshopPageTwoComponent } from './interactive-routing/workshop-page-two/workshop-page-two.component';
-import { WorkshopPageOneComponent } from './interactive-routing/workshop-page-one/workshop-page-one.component';
-import { APP_ROUTES } from './app.routes';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    WorkshopPageOneComponent,
-    WorkshopPageTwoComponent,
-    WorkshopAgendaComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(APP_ROUTES)
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-
-
 export class AppModule { }
